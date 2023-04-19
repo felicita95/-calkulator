ae8237c322cc0e65bdc4f140877d893bd5d84ef8
felicita95 <131307593+felicita95@users.noreply.github.com>
2023-04-20 00:41:32 +0300

    Update calculator.java

--- src/calculator.java
+++ src/calculator.java
@@ -19,20 +19,21 @@ public class calculator {
         if (operands.length != 2) throw new Exception("Должно быть два операнда");
         oper = detectOperation(expression);
         if (oper == null) throw new Exception("Неподдерживаемая математическая операция");
-        //если оба числа римские
+       
         if (Roman.isRoman(operands[0]) && Roman.isRoman(operands[1])) {
-            //конвертируем оба числа в арабские для вычесления действия
+          
             num1 = Roman.convertToArabian(operands[0]);
             num2 = Roman.convertToArabian(operands[1]);
             isRoman = true;
         }
-        //если оба числа арабские
+      
+        
         else if (!Roman.isRoman(operands[0]) && !Roman.isRoman(operands[1])) {
             num1 = Integer.parseInt(operands[0]);
             num2 = Integer.parseInt(operands[1]);
             isRoman = false;
         }
-        //если одни число римское, а другое - арабское
+        
         else {
             throw new Exception("Числа должны быть в одном формате");
         }
@@ -41,17 +42,17 @@ public class calculator {
         }
         int arabian = calc(num1, num2, oper);
         if (isRoman) {
-            //если римское число получилось меньше либо равно нулю, генерируем ошибку
+           
             if (arabian <= 0) {
                 throw new Exception("Римское число должно быть больше нуля");
             }
-            //конвертируем результат операции из арабского в римское
+           
             result = Roman.convertToRoman(arabian);
         } else {
-            //Конвертируем арабское число в тип String
+           
             result = String.valueOf(arabian);
         }
-        //возвращаем результат
+       
         return result;
     }
 
