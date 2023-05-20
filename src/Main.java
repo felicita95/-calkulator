dd3a845319937830fe111f203f899985d976b270
felicita95 <131307593+felicita95@users.noreply.github.com>
2023-05-21 00:23:35 +0300

    Create README.md

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        int bold = 7;
-        boolean t = bold<10;
-        System.out.println(t);
-    }
-}
--- src/newnew.java
+++
@@ -1,8 +0,0 @@
-public class newnew {
-    public static void main(String[] args) {
-        int r = 0;
-        while (r<3) {
-            System.out.println("Тяпочка");
-            r = r +1; }
-    }
-}
